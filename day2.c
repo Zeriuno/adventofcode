a244569322b167010a2aef6351ecf5c6ce53ed5a
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-01-01 14:35:38 +0100

    Saisie nom fichier, prise en conte du rab, meilleur déboggage, cosmétique

--- day2.c
+++ day2.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
 main()
 {
-  FILE *f1                    ;
-  int h, l, w, surf, paper    ;
+  FILE *f1                                 ;
+  int h, l, w, surf, paper, slack          ;
+  char input[20]                           ;
 
-  f1 = fopen ("day2.txt", "r")  ;
-  paper = 0                   ;
+  printf("Name the file with the input :") ;
+  scanf("%s", input)                       ;
+
+  f1 = fopen (input, "r")                  ;
+  paper = 0                                ;
 
   while(!feof(f1))
   {
-    fscanf(f1, "%dx%dx%d", &h, &l, &w)   ;
-    surf = (2*h*l) + (2*h*w) + (2*l*w)   ;
-    paper += surf                        ;
+    fscanf(f1, "%dx%dx%d", &h, &l, &w)     ;
+    surf = (2*h*l) + (2*h*w) + (2*l*w)     ;
+    if ((2*h*l) < (2*h*w))
+    {
+      if ((2*h*l) < (2*l*w))
+        slack = (2*h*l)                    ;
+      else
+        slack = (2*l*w)                    ;
+    }
+    else
+    {
+      if ((2*h*w) < (2*l*w))
+        slack = (2*h*w)                    ;
+      else
+        slack = (2*l*w)                    ;
+    }
+    paper += surf + slack                  ;
 
    /*printf de deboggage*/
-    printf("(2 × %d × %d) + (2 × %d × %d) + (2 × %d × %d) = %d\n", h, l, h, w, l, w, surf) ;
+    printf("(2 × %d × %d) + (2 × %d × %d) + (2 × %d × %d) = %d + %d\n", h, l, h, w, l, w, surf, slack) ;
   }
-  fclose(f1) ;
-  printf("Surface = %d\n", paper)        ;
+  fclose(f1)                               ;
+  printf("Surface = %d\n", paper)          ;
 }
