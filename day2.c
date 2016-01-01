96f301f7b0ee0d37bcf566e480ef4be10b9dba30
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-01-01 20:55:31 +0100

    Solved!
    1. Slack is not double minimal surface, but just minimal surface!
    2. Bypassed segfault by un-modularized code.
    3. workaround for the double last item with a simple substraction. Ugly but effective.

--- day2.c
+++ day2.c
@@ -1,68 +1,42 @@
 #include <stdio.h>
 
-char input_file()    ;
-char file[20]        ;
-
-
-{
-  printf("What's the name of the instructions file?: ")       ;
-  scanf("%s", file)                                           ;
-
 main()
 {
   FILE *f1                                  ;
   int h, l, w, surf, paper, slack           ;
-  char input[20]                            ;
+  char file[20]                             ;
 
-  file[20] = input_file()                   ;
-  f1 = fopen (input, "r")                   ;
+
+  printf("What's the name of the instructions file? ")        ;
+  scanf("%s", file)                                           ;
+  f1 = fopen (file, "r")                    ;
   paper = 0                                 ;
 
   while(!feof(f1))
   {
     fscanf(f1, "%dx%dx%d", &h, &l, &w)      ;
     surf = (2*h*l) + (2*h*w) + (2*l*w)      ;
-    if ((2*h*l) < (2*h*w))
+    if ((h*l) < (h*w))
     {
-      if ((2*h*l) < (2*l*w))
-        slack = (2*h*l)                     ;
+      if ((h*l) < (l*w))
+        slack = (h*l)                     ;
       else
-        slack = (2*l*w)                     ;
+        slack = (l*w)                     ;
     }
     else
     {
-      if ((2*h*w) < (2*l*w))
-        slack = (2*h*w)                     ;
+      if ((h*w) < (l*w))
+        slack = (h*w)                     ;
       else
-        slack = (2*l*w)                     ;
+        slack = (l*w)                     ;
     }
     paper += surf + slack                   ;
 
-   /*printf de deboggage*/
-    printf("(2 × %d × %d) + (2 × %d × %d) + (2 × %d × %d) = %d + %d\n", h, l, h, w, l, w, surf, slack) ;
+   /*
+    debug printf
+    printf("(2 × %d × %d) + (2 × %d × %d) + (2 × %d × %d) = %d + %d\n", h, l, h, w, l, w, surf, slack) ;*/
   }
   fclose(f1)                                ;
-/*  paper = paper - surf - slack              ;*/
+  paper = paper - surf - slack              ;
   printf("Surface = %d\n", paper)           ;
 }
-
-
-/*---------------------------------------------------------
---                     input_file                        --
---                                                       --
---                                                       --
---                                                       --
---                                                       --
---                                                       --
---                                                       --
---                                                       --
----------------------------------------------------------*/
-
-
-char input_file()
-{
-  printf("What's the name of the instructions file?: ")       ;
-  scanf("%s", file)                                           ;
-  return file[20]                                             ;
-}
-
