ec0e45ee3e7691244ba016633f69cdb2f3b7009d
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-01-05 03:09:31 +0100

    Ouverture du ficher, boucle while

--- day3-prog.c
+++ day3-prog.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
 
+void input_file() ;
+char file[20]     ;
+
+
 main()
 {
-  int map[100][100] ;
-  
+  int map[1000][1000]       ;
+  char c                    ;
+  FILE *f1                  ;
+
+
   printf("We got a map!\n") ;
+
+  input_file()              ;
+  f1 = fopen(file, "r")     ;
+
+  while((c=fgetc(f1)) != EOF)
+  {
+    printf("Bonne année!\n") ;
+  }
+
+  printf("Enfin!\n") ;  
+
 }
+
+/*---------------------------------------------------------
+--                     input_file                        --
+--                                                       --
+--                                                       --
+--                                                       --
+--                                                       --
+--                                                       --
+--                                                       --
+--                                                       --
+---------------------------------------------------------*/
+
+
+void input_file()
+{
+  printf("What's the name of the instructions file? ") ;
+  scanf("%s", file)                                    ;
+}
+
