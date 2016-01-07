d5daf15aa5ea831452c318a66024e8f11dd525dc
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-01-07 23:52:55 +0100

    Algorithm to C program - first step, declarations

---
+++ day3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main()
+{
+  FILE *f1             ;
+  char c               ;
+  int cn, i, j, found  ;
+  int itinerary[10000] ;
+  int places[10000][2] ;
+
+  printf("We've got a map!\n") ;
+}
