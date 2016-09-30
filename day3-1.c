1008d7e5bf688055f8cae4d84cc5abd72b387c87
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-09-30 14:58:16 +0200

    Dirty solution to day3 part 1

--- day3-1.c
+++ day3-1.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 
-void check()  ;
-void move()   ;
+void check()            ;
+void input_file()       ;
+void move()             ;
+
+char file[20]           ;
+int la = 0, lo = 0      ;
+int coordinates[2][9000] ;
+int houses = 0          ; 	// Starts at 1 because the starting point is already a house served.
+FILE *f1                ;
+
 main()
-{
-	int la = 0, lo = 0      ;
-	int cordinates[2][1000] ;
-	int houses = 0          ;
+{	
+	
+	char c       ;
 
-	char c                  ;
-
-	/*get the file and open it*/
-	while(!feof)
+	input_file() ;
+	
+	f1 = fopen(file, "r") ;
+	while(!feof(f1))
 	{
 		check() ; /*Starting at 0,0*/
-		/*read a char*/
-		/*change coordinates*/
+		move()  ;
 	}
 	/*close the file*/
-	printf("Santa visited this many houses : %d", houses) ;
+	printf("Santa visited this many houses : %d\n", houses) ;
 	return 0 ;
 }
 
@@ -27,21 +33,62 @@ void check()
 		int i = 0              ;
 		while(i < houses)
 		{
-			if!((coordinates[0][i] == la ) && (coordinates[1][i] == lo )) //IF coordinates are not in the cell
-				i++                     ; //Keep on checking the table
+		//	printf("houses: %d\n", houses);
+		//	printf("i     : %d\n", i)     ;
+			if(!((coordinates[0][i] == la ) && (coordinates[1][i] == lo ))) //if coordinates are not in the cell
+			{
+		//		printf("la = %d\ncoordinates[0][%i]=%d\n", la, i, coordinates[0][i]) ;
+		//		printf("lo = %d\ncoordinates[1][%i]=%d\n", lo, i, coordinates[1][i]) ;
+				i++                     ; 								  //Keep on checking the table
+			}
 			else
-				i = houses + 1          ; //Else quit the loop
+				i = houses + 1          ; 								  //else quit the loop
 		}
-		if(i > houses) //If we quit the loop because of the 'else', we add to the table the current coordinates
+		//printf("After check\n");
+		//printf("houses: %d\n", houses);
+		//printf("i     : %d\n", i)     ;
+		if(i == houses) //if we quit the loop because of the 'else', we add to the table the current coordinates
 		{
-			coordinates[0][houses] = la ;
-			coordinates[1][houses] = lo ;
-			houses++                    ;
+			coordinates[0][houses] = la           ;
+			coordinates[1][houses] = lo           ;
+			houses++                              ;
+			printf("Served %d houses!\n", houses) ;
 		}
 	}
-void move()
+
+void move() //what happens when you move
 {
-	//what happens when you move
+	char c ;
+	fscanf(f1, "%c"	, &c) ;
+	// printf("I read '%c'\n", c) ;
+	switch(c)
+	{
+		case '^':
+			la++  ;
+		//	printf("la = %d\nlo = %d\n", la, lo) ;
+			break ;
+		case 'v':
+			la--  ;
+		//	printf("la = %d\nlo = %d\n", la, lo) ;
+			break ;
+		case '<':
+			lo--  ;
+		//	printf("la = %d\nlo = %d\n", la, lo) ;
+			break ;
+		case '>':
+			lo++  ;
+		//	printf("la = %d\nlo = %d\n", la, lo) ;
+			break ;
+		default:
+		//	printf("Mhh, bumpy road: %c\n", c) ;
+			break ;
+	}
+}
+
+void input_file()
+{
+  printf("What's the name of the instructions file? ")       ;
+  scanf("%s", file)                                           ;
 }
 
 /*
@@ -134,3 +181,4 @@ main ()
 	printf("Fait.\n");
 	return 0 ;
 }
+*/
