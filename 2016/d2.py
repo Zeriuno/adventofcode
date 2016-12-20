29b0aebc205ec37c302ed4aaf26a68b12e9869bf
Zeriuno <d.pitrolo@gmx.com>
2016-12-20 15:33:05 +0100

    Without going variable

--- 2016/d2.py
+++ 2016/d2.py
@@ -24,21 +24,25 @@ def get_code(param):
 	for instruction in instructions:
 		for move in instruction:
 			if(move == 'U'):
-				going = finger[1] + 1
-				if(going <= 2):
-					finger = (finger[0], going)
+				if (finger[1] == 2):
+					pass
+				else:
+					finger = (finger[0],finger[1] + 1)
 			elif(move == 'D'):
-				going = finger[1] - 1
-				if(going >= 0 ):
-					finger = (finger[0], going)
+				if (finger[1] == 0):
+					pass
+				else:
+					finger = (finger[0], finger[1] - 1)
 			elif(move == 'L'):
-				going = finger[0] - 1
-				if(going >= 0):
-					finger = (going, finger[1])
+				if (finger[0] == 0):
+					pass
+				else:
+					finger = (finger[0] - 1, finger[1])
 			elif(move =='R'):
-				going = finger[0] + 1
-				if(going <= 2):
-					finger = (going, finger[1])
+				if (finger[0] == 2):
+					pass
+				else:
+					finger = (finger[0] + 1, finger[1])
 		code += str(press(finger))
 	print(code)
 	instructions.close()
