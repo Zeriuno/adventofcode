21840c229cf2ff478e09f9497314502abf6130b3
Zeriuno <d.pitrolo@gmx.com>
2017-01-02 13:09:05 +0100

    Defining the Room class

--- 2016/d4.py
+++ 2016/d4.py
@@ -4,14 +4,36 @@ def get_ID_tot(param):
 	"""
 	tot_ID = 0
 	list = open(param, "r")
-	for room in list:
-		if (room.is_real):
+	for r in list:
+		r = Room(r)
+		if (r.is_real):
 			tot_ID += room.ID
 	return tot_ID
 
 
-1. Take a room name.
-2. Find the 5 most frequent letters of the name
+class Room:
+	"""
+	Room object.
+	"""
+
+	def __init__(self):
+		"""
+		Creation of a Room.
+		room1 = Room(aaaaa-bbb-z-y-x-123[abxyz])
+		"""
+		self.checksum = self.split('[')[-1][:-1]
+		self.ID = int(self.split('-')[-1].split('[')[0])
+		self.string = ''
+		for bit in self.split('-')[:-1]:
+			self.string += bit
+		self.occurrences = collection.Counter(self.string)
+
+
+
 2.b Order the ties alphabetically
-3. Check the 5 most frequent letters appear in alphabetical order
+3. Check the 5 most frequent letters appear in alphabetical order: self.is_real
+
+1. Take a room name.
+
+
 4. If that's a real room, add the sector ID to the total.
